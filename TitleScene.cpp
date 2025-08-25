d6039e564847df08c30cee445014ee344fe39061
230645 <230645@E0404-18>
2025-08-25 13:00:32 +0900

    音を追加したよ

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,16 +1,23 @@
 #include "TitleScene.h"
 #include "Engine/Input.h"
 #include "Engine/Image.h"
+#include "Engine/Audio.h"
 #include "Engine/SceneManager.h"
 TitleScene::TitleScene(GameObject* parent)
-	:GameObject(parent,"TitleScene"),hImege_(-1),select_(0),prevUpKey_(false),prevDownKey_(false)
+	:GameObject(parent,"TitleScene")
+	,hImege_(-1)
+	,select_(0),prevUpKey_(false),prevDownKey_(false),hSound_(-1)
 {
 }
 
 void TitleScene::Initialize()
 {
+	//Title画像のロード
 	hImege_ = Image::Load("Model\\EggHunterGameTitle.png");
 	assert(hImege_ >= 0);
+	//サウンドデータのロード
+	hSound_ = Audio::Load("Model\\fanfare.wav");
+	assert(hSound_ >= 0);
 }
 
 void TitleScene::Update()
@@ -26,6 +33,8 @@ void TitleScene::Draw()
 {
 	Image::SetTransform(hImege_, transform_);
 	Image::Draw(hImege_);
+
+	Audio::Play(hSound_);
 }
 
 void TitleScene::Release()
--- TitleScene.h
+++ TitleScene.h
@@ -7,6 +7,7 @@ class TitleScene :
 	int select_;//選択
 	int prevUpKey_;//前フレームの上キー
 	int prevDownKey_;//前フレームの下キー
+	int hSound_;//サウンド番号
 
 public:
 	//コンストラクタ
